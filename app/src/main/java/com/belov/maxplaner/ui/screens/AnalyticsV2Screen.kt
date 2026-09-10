e55b129e9a328f6de431a4cb199e12e2e507ec02
gbelov8841 <gbelov8841@gmail.com>
2026-09-10 23:21:30 +0300

    Fix progress screen dp import

--- app/src/main/java/com/belov/maxplaner/ui/screens/AnalyticsV2Screen.kt
+++ app/src/main/java/com/belov/maxplaner/ui/screens/AnalyticsV2Screen.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.belov.maxplaner.data.PlannerStore
 import com.belov.maxplaner.data.bestScheduledHabitStreak
 import com.belov.maxplaner.ui.components.PlannerCard
