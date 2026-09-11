2c7b9aded3df513fecb68c4ab05bcf560e5980b6
gbelov8841 <gbelov8841@gmail.com>
2026-09-11 09:26:32 +0300

    Clean Floating Glass app imports

--- app/src/main/java/com/belov/maxplaner/ui/MaxPlanerApp.kt
+++ app/src/main/java/com/belov/maxplaner/ui/MaxPlanerApp.kt
@@ -43,7 +43,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.platform.LocalContext
